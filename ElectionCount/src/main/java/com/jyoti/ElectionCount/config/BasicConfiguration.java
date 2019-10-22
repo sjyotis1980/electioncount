f9f51aaefb6c61bc7dcb172e9dce3b15c7f66b33
Jyoti Kumar <s.jyoti.s1980@gmail.com>
2019-10-22 12:44:11 +0530

    JUnit test result for one method - 22-October-2019

--- ElectionCount/src/main/java/com/jyoti/ElectionCount/config/BasicConfiguration.java
+++ ElectionCount/src/main/java/com/jyoti/ElectionCount/config/BasicConfiguration.java
@@ -7,37 +7,30 @@ package com.jyoti.ElectionCount.config;
 
 
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
 
 /**
  * @author JyotiKumar
  *
  */
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 public class BasicConfiguration extends WebSecurityConfigurerAdapter {
 	
 	
-	@SuppressWarnings("deprecation")
-	@Bean
-    public UserDetailsService userDetailsService() {
-
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(users.username("user").password("password").roles("USER").build());
-        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
-        return manager;
-
-    }
-	
+    @Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance()).withUser("user").password("pwd").roles("USER")
+		                                                                                .and().withUser("admin").password("admin").roles("USER", "ADMIN");
+	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
--- ElectionCount/src/test/java/com/jyoti/ElectionCount/ElectionCountApplicationTests.java
+++ ElectionCount/src/test/java/com/jyoti/ElectionCount/ElectionCountApplicationTests.java
@@ -1,16 +1,64 @@
 package com.jyoti.ElectionCount;
 
+import static org.junit.Assert.assertEquals;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
+
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ElectionCountApplicationTests {
 
+	private MockMvc mockMvc;
+
+	@LocalServerPort
+	private int port;
+
+	private String getRootUrl() {
+		return "http://localhost:" + port;
+	}
+
+	@Autowired
+	private TestRestTemplate template;
+
+	@Autowired
+	private WebApplicationContext context;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+	}
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<?> response = template.withBasicAuth("admin", "admin").exchange(getRootUrl() + "/api/test/load",
+				HttpMethod.GET, entity, String.class);
+
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+
 	}
 
 }
